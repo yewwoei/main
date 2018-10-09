a09a16017707e7f44e4ac35000faa08bbbe2b0f5
Evan Mok <31198642+evanmok2401@users.noreply.github.com>
2018-10-09 09:47:54 +0800

    Fix Style Errors

--- src/main/java/seedu/address/storage/XmlSerializableUsers.java
+++ src/main/java/seedu/address/storage/XmlSerializableUsers.java
@@ -6,8 +6,8 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 import seedu.address.commons.exceptions.IllegalValueException;
-import seedu.address.model.user.Friendship;
 import seedu.address.model.UserData;
+import seedu.address.model.user.Friendship;
 import seedu.address.model.user.User;
 
 /**
