3d94400ffed85bbcab2c368f560e78bdf51995bc
evanmok2401 <evanmok2401@gmail.com>
2018-10-10 01:14:45 +0800

    Updated CliSyntax. Add the SignUp Command in AddressBookParser. Add SignUpCommandParser.

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -19,6 +19,7 @@ import seedu.address.logic.commands.ListCommand;
 import seedu.address.logic.commands.RedoCommand;
 import seedu.address.logic.commands.SelectCommand;
 import seedu.address.logic.commands.UndoCommand;
+import seedu.address.logic.commands.user.SignUpCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
@@ -83,6 +84,9 @@ public class AddressBookParser {
 
         case RedoCommand.COMMAND_WORD:
             return new RedoCommand();
+
+        case SignUpCommand.COMMAND_WORD: 
+            return new SignUpCommandParser().parse(arguments);
 
         // Restaurant Commands
 
