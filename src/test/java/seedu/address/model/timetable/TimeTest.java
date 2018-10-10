217870402c99e912b08158716f71d2117574ff05
Meena567 <meenakshi6769@gmail.com>
2018-10-10 12:42:39 +0800

    fix style in time test

--- src/test/java/seedu/address/model/timetable/TimeTest.java
+++ src/test/java/seedu/address/model/timetable/TimeTest.java
@@ -1,13 +1,13 @@
 package seedu.address.model.timetable;
 
-import static org.junit.Assert.assertEquals;
+/*import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static seedu.address.model.timetable.DateTestUtil.VALID_TIME_END;
 import static seedu.address.model.timetable.DateTestUtil.VALID_TIME_MID;
 import static seedu.address.model.timetable.DateTestUtil.VALID_TIME_START;
 import static seedu.address.model.timetable.DateTestUtil.INVALID_TIME_END;
-import static seedu.address.model.timetable.DateTestUtil.INVALID_TIME_MID;
+import static seedu.address.model.timetable.DateTestUtil.INVALID_TIME_MID;*/
 
 import org.junit.Rule;
 import org.junit.Test;
