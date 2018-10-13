5c3da46f4297477911a4d09696fce9c5a81e3a9e
Ng Hoi Ying, Kate <38391895+katenhy@users.noreply.github.com>
2018-10-14 03:44:11 +0800

    Update ListDebtHistoryCommandParser.java

--- src/main/java/seedu/address/logic/parser/accounting/ListDebtHistoryCommandParser.java
+++ src/main/java/seedu/address/logic/parser/accounting/ListDebtHistoryCommandParser.java
@@ -4,6 +4,9 @@ import seedu.address.logic.commands.accounting.ListDebtHistoryCommand;
 import seedu.address.logic.parser.Parser;
 import seedu.address.logic.parser.exceptions.ParseException;
 
+/**
+ * Parses input arguments and creates a new ListDebtHistoryCommand object
+ */
 public class ListDebtHistoryCommandParser implements Parser<ListDebtHistoryCommand> {
     @Override
     public ListDebtHistoryCommand parse(String userInput) throws ParseException {
