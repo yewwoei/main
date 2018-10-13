d44d66bccb4802f16f755591290f04ee88ceecfb
Ng Hoi Ying, Kate <38391895+katenhy@users.noreply.github.com>
2018-10-14 03:45:26 +0800

    Update ListDebtorCommandParser.java

--- src/main/java/seedu/address/logic/parser/accounting/ListDebtorCommandParser.java
+++ src/main/java/seedu/address/logic/parser/accounting/ListDebtorCommandParser.java
@@ -4,6 +4,9 @@ import seedu.address.logic.commands.accounting.ListDebtorCommand;
 import seedu.address.logic.parser.Parser;
 import seedu.address.logic.parser.exceptions.ParseException;
 
+/**
+ * Parses input arguments and creates a new ListDebtorCommand object
+ */
 public class ListDebtorCommandParser implements Parser<ListDebtorCommand> {
     @Override
     public ListDebtorCommand parse(String userInput) throws ParseException {
