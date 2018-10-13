bf043be3a8d82427f461e9af0e35f5a0b5dd2734
Ng Hoi Ying, Kate <38391895+katenhy@users.noreply.github.com>
2018-10-14 03:44:44 +0800

    Update ListDebtRequestReceivedCommandParser.java

--- src/main/java/seedu/address/logic/parser/accounting/ListDebtRequestReceivedCommandParser.java
+++ src/main/java/seedu/address/logic/parser/accounting/ListDebtRequestReceivedCommandParser.java
@@ -4,6 +4,9 @@ import seedu.address.logic.commands.accounting.ListDebtRequestReceivedCommand;
 import seedu.address.logic.parser.Parser;
 import seedu.address.logic.parser.exceptions.ParseException;
 
+/**
+ * Parses input arguments and creates a new ListDebtRequestReceivedCommand object
+ */
 public class ListDebtRequestReceivedCommandParser implements Parser<ListDebtRequestReceivedCommand> {
     @Override
     public ListDebtRequestReceivedCommand parse(String userInput) throws ParseException {
