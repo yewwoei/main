a7fe0b862b69e58ac95d58861420f2b85b212dbc
katenhy <>
2018-10-23 19:28:26 +0800

    style fixing

--- src/main/java/seedu/address/logic/LogicManager.java
+++ src/main/java/seedu/address/logic/LogicManager.java
@@ -11,7 +11,6 @@ import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.AddressBookParser;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.Model;
-import seedu.address.model.jio.Jio;
 import seedu.address.model.restaurant.Restaurant;
 
 /**
