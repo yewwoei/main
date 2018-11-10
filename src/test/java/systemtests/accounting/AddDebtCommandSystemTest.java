fc4fbf4bf2eef6c530141a7558380282eed93e52
katenhy <>
2018-11-11 04:30:10 +0800

    fix style

--- src/test/java/systemtests/accounting/AddDebtCommandSystemTest.java
+++ src/test/java/systemtests/accounting/AddDebtCommandSystemTest.java
@@ -224,7 +224,7 @@ public class AddDebtCommandSystemTest extends AddressBookSystemTest {
      * Added a millie second sleep.
      * Use to prevent fast execution which generate two debt with same id in a millie second.
      */
-    private void addSleep(){
+    private void addSleep() {
         try {
             Thread.sleep(1);
         } catch (InterruptedException e) {
