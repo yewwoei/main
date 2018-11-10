6809e34fe49ba8653da75b68a376a0122cc535d5
chelchia <chelseachiajh98@gmail.com>
2018-11-10 16:14:27 +0800

    create jio command integration test

---
+++ src/test/java/seedu/address/logic/commands/jio/CreateJioCommandIntegrationTest.java
@@ -0,0 +1,48 @@
+package seedu.address.logic.commands.jio;
+
+import org.junit.Before;
+import org.junit.Test;
+import seedu.address.logic.CommandHistory;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.jio.Jio;
+
+import seedu.address.testutil.TypicalUsers;
+import seedu.address.testutil.UserBuilder;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.model.jio.JioTestUtil.MALA;
+import static seedu.address.testutil.TypicalRestaurants.getTypicalAddressBook;
+
+public class CreateJioCommandIntegrationTest {
+    private Model model;
+    private CommandHistory commandHistory = new CommandHistory();
+
+    @Before
+    public void setUp() {
+        model = new ModelManager(getTypicalAddressBook(), new UserPrefs(), TypicalUsers.getTypicalUserData(),
+                new UserBuilder().build());
+    }
+
+    @Test
+    public void execute_newJio_success() {
+        Jio validJio = MALA;
+
+        Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs(),
+                TypicalUsers.getTypicalUserData(), new UserBuilder().build());
+        expectedModel.createJio(validJio);
+
+        assertCommandSuccess(new CreateJioCommand(validJio), model, commandHistory,
+                String.format(CreateJioCommand.MESSAGE_SUCCESS, validJio), expectedModel);
+    }
+
+    @Test
+    public void execute_duplicateRestaurant_throwsCommandException() {
+        Jio jioInList = model.getJioList().get(0);
+        assertCommandFailure(new CreateJioCommand(jioInList), model, commandHistory,
+                CreateJioCommand.MESSAGE_DUPLICATE_JIO);
+    }
+
+}
