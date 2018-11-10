f32cd633c7948794d83e9a6e9268dc4c334b2dda
Meena567 <meenakshi6769@gmail.com>
2018-11-10 12:44:00 +0800

    start integration test

---
+++ src/test/java/seedu/address/logic/commands/group/AddFriendCommandIntegrationTest.java
@@ -0,0 +1,7 @@
+package seedu.address.logic.commands.group;
+
+/**
+ * Contains integration tests (interaction with the Model) for {@code AddFriendCommand}.
+ */
+public class AddFriendCommandIntegrationTest {
+}
