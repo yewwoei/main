32c6862e791ef64670cb485bd19361f7eb26715d
yewwoei <yewwoei.low@outlook.com>
2018-11-10 17:50:44 +0800

    add datebuilder and blockdatecommand tests

--- src/test/java/seedu/address/logic/commands/timetable/BlockDateCommandTest.java
+++ src/test/java/seedu/address/logic/commands/timetable/BlockDateCommandTest.java
@@ -1,14 +1,24 @@
 package seedu.address.logic.commands.timetable;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.testutil.TypicalRestaurants.getTypicalAddressBook;
+import static seedu.address.testutil.TypicalDates.DATE_A;
 
 import org.junit.Test;
 
+import seedu.address.commons.core.Messages;
 import seedu.address.logic.CommandHistory;
+import seedu.address.logic.commands.RedoCommand;
+import seedu.address.logic.commands.UndoCommand;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserData;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.timetable.Date;
+import seedu.address.testutil.UserBuilder;
 
 /**
  * Contains integration tests (interaction with the Model, UndoCommand, and RedoCommand) and unit tests for
@@ -16,30 +26,53 @@ import seedu.address.model.UserPrefs;
  */
 public class BlockDateCommandTest {
 
-    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs(), new UserData());
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs(), new UserData(), new UserBuilder().build());
     private CommandHistory commandHistory = new CommandHistory();
 
     @Test
     public void execute_validDate_success() {
-    }
+        Date dateToBlock = DATE_A;
+        BlockDateCommand blockDateCommand = new BlockDateCommand(dateToBlock);
 
-    @Test
-    public void execute_invalidDate_throwsCommandException() {
+        String expectedMessage = String.format(BlockDateCommand.MESSAGE_SUCCESS, dateToBlock);
 
-    }
+        ModelManager expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs(), new UserData(),
+                new UserBuilder().build());
 
-    @Test
-    public void execute_withoutLogin_throwsNotLoggedInCommandException() {
+        expectedModel.blockDateForCurrentUser(dateToBlock);
+        expectedModel.commitAddressBook();
+
+        assertCommandSuccess(blockDateCommand, model, commandHistory, expectedMessage, expectedModel);
+        model.undoAddressBook();
 
     }
 
     @Test
     public void execute_duplicateDate_throwsCommandException() {
+        Date dateToBlock = DATE_A;
+        model.blockDateForCurrentUser(dateToBlock);
 
+        BlockDateCommand blockDateCommand = new BlockDateCommand(dateToBlock);
+
+        assertCommandFailure(blockDateCommand, model, commandHistory,
+                BlockDateCommand.MESSAGE_DUPLICATE_DATE);
     }
 
+
     @Test
-    public void executeUndoRedo_validDate_success() throws Exception {
+    public void execute_withoutLogin_throwsNotLoggedInCommandException() {
+        Date dateToBlock = DATE_A;
+        ModelManager notLoggedInModel = new ModelManager(model.getAddressBook(), new UserPrefs(), new UserData());
+
+        BlockDateCommand blockDateCommand = new BlockDateCommand(dateToBlock);
+        assertCommandFailure(blockDateCommand, notLoggedInModel, commandHistory,
+                String.format(Messages.MESSAGE_USER_NOT_LOGGED_IN_FOR_COMMAND, BlockDateCommand.COMMAND_WORD));
+    }
+
+
+    @Test
+    public void executeUndoRedo_validDate_sameDateDeleted() throws Exception {
+
 
     }
 
